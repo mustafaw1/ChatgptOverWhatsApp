216213d1de6ddf5e1b28f98c865eaddfb0e7b5d4
mustafaw1 <mustafazaidi840@gmail.com>
2023-03-12 00:07:33 +0500

    Refactor: handle some error using openai excetpion

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import openai
+from settings import DEFAULT_OPENAPI_REPLY
 
 app = Flask(__name__)
-openai.api_key = ' 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'
+openai.api_key = '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'
+# conig()
 
-def get_answere(question):
-    #get the response back from the chatgpt
-    completions = openai.Completion.create(
-        model = 'text-davinci-003',   #ID of the model to use
-        temperature = 0.5,            #what sampling temperature to use
-        prompt = question,            #questions
-        max_tokens = 100,             #size of the buffer actually
-        n = 1,                        #how many completion generate from each prompt
-        stop = None,                  #where the api stop generating further token
-    )
-    #fectch and return the text
+
+def get_answer(question):
+    try:
+        # get the response back from the chatgpt
+        completions = openai.Completion.create(
+            model = 'text-davinci-003',   #ID of the model to use
+            temperature = 0.5,            #what sampling temperature to use
+            prompt = question,            #questions
+            max_tokens = 100,             #size of the buffer actually
+            n = 1,                        #how many completion generate from each prompt
+            stop = None,                  #where the api stop generating further token
+        )
+    except openai.error.RateLimit:
+        return DEFAULT_OPENAPI_REPLY
+
+    #fetch and return the text
     return completions.choices[0].text
 
 
@@ -25,8 +32,9 @@ def whatsapp_reply():
     print('User query: ', question)
     twilio_response = MessagingResponse()
     reply = twilio_response.message()
+
     #generate response using chatgpt
-    answere = get_answere(question)
+    answere = get_answer(question)
     # send back answere as a whatsapp reply
     reply.body(answere)
     #send the response back via whatsapp
---
+++ settings.py
@@ -0,0 +1,5 @@
+from decouple import config
+
+OPENAPI_KEY = config("OPENAPI_KEY")
+
+DEFAULT_OPENAPI_REPLY = "Sorry ,a system error occurred"
