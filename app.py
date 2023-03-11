213dd7b372c3283c69deb39a6e9005e8ce431934
mustafaw1 <mustafazaidi840@gmail.com>
2023-03-11 17:49:41 +0500

    Integrated chatgpt to twilio generated whatsapp number

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import openai
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return "Hello worl"
+openai.api_key = ' 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'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def get_answere(question):
+    #get the response back from the chatgpt
+    completions = openai.Completion.create(
+        model = 'text-davinci-003',   #ID of the model to use
+        temperature = 0.5,            #what sampling temperature to use
+        prompt = question,            #questions
+        max_tokens = 100,             #size of the buffer actually
+        n = 1,                        #how many completion generate from each prompt
+        stop = None,                  #where the api stop generating further token
+    )
+    #fectch and return the text
+    return completions.choices[0].text
+
+
+@app.route("/whatsapp", methods=['POST'])
+def whatsapp_reply():
+    question = request.form.get('Body')
+    print('User query: ', question)
+    twilio_response = MessagingResponse()
+    reply = twilio_response.message()
+    #generate response using chatgpt
+    answere = get_answere(question)
+    # send back answere as a whatsapp reply
+    reply.body(answere)
+    #send the response back via whatsapp
+    return str(twilio_response)
+
+
